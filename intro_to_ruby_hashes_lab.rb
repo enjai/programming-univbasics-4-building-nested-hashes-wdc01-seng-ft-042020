7826fcc9e4f52c65acbc327553ac6e4ef133df8f
Injae Lee <ilee.injae@gmail.com>
2020-04-02 06:31:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
 
  {
    railroads: {
- pieces: 4, rent_in_dollars: {one_piece_owned: 25}, names: {}
+ pieces: 4, rent_in_dollars: {one_piece_owned: 25, two_piece_owned: 50}, names: {}
      
    }
  }
